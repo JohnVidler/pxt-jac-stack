217e24e5604825c80ba53062d5314892089d1b8f
Dr John Vidler <j.vidler@lancaster.ac.uk>
2023-06-08 15:04:40 +0100

    Merging in changes from the test project

--- main.ts
+++ main.ts
@@ -1,26 +1,42 @@
+enum LocationFixType {
+    //% block="Offline"
+    OFFLINE = 0,
 
+    //% block="No Fix"
+    NONE = 1,
+
+    //% block="2D Fix"
+    FIX_2D = 2,
+
+    //% block="3D Fix"
+    FIX_3D = 3
+};
 
 //% block="jac:stack"
 //% color="#a0b4d2"
 namespace pxt_jac_stack {
 
-    enum FixType {
-        NONE = -1,
-        FIX_2D,
-        FIX_3D
-    };
-
-    let _serialBuffer = "";
-    let _seenUpdate = false
-    let _i2cAddress = 0x10; // Default for the MTK3339 'Ultimate GPS' module
+    let _monitorEnable = false;
     let _uartBaud = 9600;
     let _uartTx = SerialPin.P1;
     let _uartRx = SerialPin.P0;
 
-    //% block="set jac:stack i2c address to $address"
-    //% advanced=true
-    export function setI2CAddress( address: number ): void {
-        _i2cAddress = address;
+    let location = {
+        datetime: {
+            hour: 0,
+            minute: 0,
+            second: 0,
+            year: 0,
+            month: 0,
+            day: 0
+        },
+        latitude: 0,
+        longitude: 0,
+        altitude: 0,
+        speedOverGround: 0,
+        courseOverGround: 0,
+        fixType: LocationFixType.NONE,
+        satellites: 0
     }
 
     //% block="set jac:stack serial pins to $tx and $rx"
@@ -35,68 +51,141 @@ namespace pxt_jac_stack {
     export function setAutoUpdates( automatic: boolean = true ): void {
         if( automatic ) {
             serial.redirect(_uartTx, _uartRx, _uartBaud);
-            serial.onDataReceived("\n", parseBuffer)
+            serial.setRxBufferSize( 128 );
+            _monitorEnable = true;
             return;
         }
 
-        serial.redirectToUSB()
+        serial.redirectToUSB();
         serial.setBaudRate(115200);
+        _monitorEnable = false;
     }
 
-    // This should block until we get a location message from the radio - note that it doesn't
-    // guarantee that there's a valid fix!
-    //
-    //% block="update my location"
-    //% group="location"
-    export function updateLocation(): void {
-        //
+    //% block="latitude"
+    //% group="GPS Location"
+    function getLatitude(): number {
+        return location.latitude;
     }
 
-    //% block="location accuracy"
-    //% group="location"
-    export function gpsAccuracy(): FixType {
-        return FixType.NONE;
+    //% block="longitude"
+    //% group="GPS Location"
+    function getLongitude(): number {
+        return location.longitude;
     }
 
-    //% block="GPS latitude"
-    //% group="location"
-    export function gpsLatitude(): number {
-        return 0;
+    //% block="altitude"
+    //% group="GPS Location"
+    function getAltitude(): number {
+        return location.altitude;
     }
 
-    //% block="GPS longitude"
-    //% group="location"
-    export function gpsLongitude(): number {
-        return 0;
+    //% block="ground speed"
+    //% group="GPS Movement"
+    function getGroundSpeed(): number {
+        return location.speedOverGround;
     }
 
-    //% block="GPS altitude"
-    //% group="location"
-    export function gpsAltitude(): number {
-        return 0;
+    //% block="ground direction"
+    //% group="GPS Movement"
+    function getCourse(): number {
+        return location.courseOverGround;
     }
 
-    //% block="current GPS time"
-    //% group="extras"
-    export function gpsTime(): number {
-        return 0;
+    //% block="satellites"
+    //% group="GPS Statistics"
+    function getSatellites(): number {
+        return location.satellites;
     }
 
-    function parseBuffer() {
-        _serialBuffer.concat(serial.readString())
+    //% block="satellites"
+    //% group="GPS Statistics"
+    function getFix(): LocationFixType {
+        return location.fixType;
+    }
 
-        // Sync to the first '$' if present
-        while (_serialBuffer.length > 0 && _serialBuffer.charAt(0) != '$')
-            _serialBuffer = _serialBuffer.slice(1);
+    control.runInBackground(() => {
+        while (true) {
+            if (_monitorEnable) {
+                let readline = serial.readLine()
+                if (readline.length > 0)
+                    parseNMEA(readline);
 
-        // Did we actually sync on anything?
-        if (_serialBuffer.charAt(0) == '$') {
-            let line = _serialBuffer.split("\n")[0];
-            parseNMEA(line);
-            _serialBuffer = _serialBuffer.slice(line.length - 1);
+                pause(5);
+            }
+            else
+                pause(200);
+        }
+    });
+
+    function parseNMEA(input: string): void {
+        let msg = input.split(",");
+
+        switch (msg[0]) {
+            case "$GPGGA": parseGPGGA(msg); break;
+            case "$GPGSA": location.fixType = parseInt(msg[2]); break;
+            case "$GPRMC": parseGPRMC(msg); break;
+            default:
+                // Skip
         }
     }
 
-    function parseNMEA( line: string ): void {
+    function parseGPRMC(msg: string[]): void {
+        if (msg[2] == 'A') {
+            location.datetime.hour = parseInt(msg[1].slice(0, 2));
+            location.datetime.minute = parseInt(msg[1].slice(2, 4));
+            location.datetime.second = parseInt(msg[1].slice(4));
+            location.datetime.year = parseInt(msg[9].slice(4, 6));
+            location.datetime.month = parseInt(msg[9].slice(2, 4));
+            location.datetime.day = parseInt(msg[9].slice(0, 2));
+
+            location.speedOverGround = parseFloat(msg[7]) * 0.514444;
+            location.courseOverGround = parseFloat(msg[8]);
+            return;
+        }
+        location.datetime.hour = 0;
+        location.datetime.minute = 0;
+        location.datetime.second = 0;
+        location.datetime.year = 0;
+        location.datetime.month = 0;
+        location.datetime.day = 0;
+
+        location.speedOverGround = 0;
+        location.courseOverGround = 0;
+    }
+
+    function parseGPGGA(msg: string[]): void {
+        if (msg.length != 15)
+            return;
+
+        location.latitude = computeLatitude(msg[2], msg[3]);
+        location.longitude = computeLongitude(msg[4], msg[5]);
+
+        // Catch NaN's so we don't blow up folks code later...
+        if (location.latitude == NaN)
+            location.latitude = 0;
+        if (location.longitude == NaN)
+            location.longitude = 0;
+
+        location.satellites = parseInt(msg[7]);
+    }
+
+    function computeLatitude(input: string, ns: string): number {
+        if (input.length < 4)
+            return 0;
+        let d = parseInt(input.slice(0, 2));
+        let m = parseFloat(input.slice(2)) / 60;
+        if (ns.toUpperCase() == "S")
+            return (d + m) * -1;
+        return (d + m);
+    }
+
+    function computeLongitude(input: string, ew: string): number {
+        if (input.length < 4)
+            return 0;
+        let d = parseInt(input.slice(0, 3));
+        let m = parseFloat(input.slice(3)) / 60;
+        if (ew.toUpperCase() == "W")
+            return (d + m) * -1;
+        return (d + m);
     }
 }
